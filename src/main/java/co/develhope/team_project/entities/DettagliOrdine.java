32f7a045a83ab9d6aad03f82452e095b778cc47e
Dennis Ciccone <dennisciccone02@gmail.com>
2025-06-13 16:16:17 +0200

    Completate le classi Abbonamento, Asta, CopiaFumetto, DettagliOrdine e IscrizioneAsta con costruttori e metodi getter e setter

--- src/main/java/co/develhope/team_project/entities/DettagliOrdine.java
+++ src/main/java/co/develhope/team_project/entities/DettagliOrdine.java
@@ -1,4 +1,62 @@
 package co.develhope.team_project.entities;
 
+import jakarta.persistence.*;
+
+@Entity
 public class DettagliOrdine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long dettagliOrdineId;
+
+    private CopiaFumetto copiaFumetto;
+
+    private Integer quantitaFumetti;
+
+    @ManyToOne
+    @JoinColumn(name = "ordine_id")
+    private Ordine ordine;
+
+    public DettagliOrdine() {
+    }
+
+    public DettagliOrdine(Long dettagliOrdineId, CopiaFumetto copiaFumetto, Integer quantitaFumetti, Ordine ordine) {
+        this.dettagliOrdineId = dettagliOrdineId;
+        this.copiaFumetto = copiaFumetto;
+        this.quantitaFumetti = quantitaFumetti;
+        this.ordine = ordine;
+    }
+
+    public Long getDettagliOrdineId() {
+        return dettagliOrdineId;
+    }
+
+    public void setDettagliOrdineId(Long dettagliOrdineId) {
+        this.dettagliOrdineId = dettagliOrdineId;
+    }
+
+    public CopiaFumetto getCopiaFumetto() {
+        return copiaFumetto;
+    }
+
+    public void setCopiaFumetto(CopiaFumetto copiaFumetto) {
+        this.copiaFumetto = copiaFumetto;
+    }
+
+    public Integer getQuantitaFumetti() {
+        return quantitaFumetti;
+    }
+
+    public void setQuantitaFumetti(Integer quantitaFumetti) {
+        this.quantitaFumetti = quantitaFumetti;
+    }
+
+    public Ordine getOrdine() {
+        return ordine;
+    }
+
+    public void setOrdine(Ordine ordine) {
+        this.ordine = ordine;
+    }
 }
+
