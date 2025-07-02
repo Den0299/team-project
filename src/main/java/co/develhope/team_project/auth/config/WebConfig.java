25ea90445750c5ae114c0855c4db71bbab723c3c
Dennis Ciccone <dennisciccone02@gmail.com>
2025-07-02 11:52:57 +0200

    Modificato l'endpoint dell'interceptor in WebConfig

--- src/main/java/co/develhope/team_project/auth/config/WebConfig.java
+++ src/main/java/co/develhope/team_project/auth/config/WebConfig.java
@@ -14,6 +14,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(jwtInterceptor).addPathPatterns("/api/**");
+        registry.addInterceptor(jwtInterceptor).addPathPatterns("/auth/me");
     }
 }
